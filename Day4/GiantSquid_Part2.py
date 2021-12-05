815d7457907594534589af456b0d67c8ac16bdb8
Caleb Simmons <cmsimmons175@gmail.com>
2021-12-05 11:13:09 -0700

    Added Day 4 Part 2

--- Day4/GiantSquid_Part2.py
+++ Day4/GiantSquid_Part2.py
@@ -58,11 +58,19 @@ picked = lines[0].split(",")
 boards = [Board.organize(lines[i + 1 : i + 6]) for i in range(1, len(lines), 6)]
 
 for number in picked:
+    temp = []
     for board in boards:
         board.check_board(number)
-        #board.print_board()
-        if board.has_won():
-            print("\nYou found the winner!\n")
-            board.print_board()
-            print("\nYour score is " + str(board.calculate_answer(int(number))) + "\n")
-            exit()
+        if (len(boards) != 1):
+            if board.has_won():
+                temp.append(board)
+        else:
+            if board.has_won():
+                print("\nYou have found the ideal loser board!\n")
+                boards[0].print_board()
+                print("\nThe answer is " + str(boards[0].calculate_answer(int(number))) + "\n")
+                exit()
+    
+    for item in temp:
+        if (len(temp) > 0):
+            boards.remove(item)
